45c9b3ecbd16c5d4378e9dfbc434ef57f07c48f3
dendrocat <dan.re0@mail.ru>
2024-11-28 12:01:32 +0300

    move signals from users to game app

--- app/game/signals.py
+++ app/game/signals.py
@@ -1,6 +1,13 @@
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
+from django.contrib.auth import get_user_model
 from .models import GameData
+
+
+@receiver(post_save, sender=get_user_model())
+def create_gamedata(sender, instance, created, **kwargs):
+    if created:
+        GameData.objects.create(user=instance)
 
 
 @receiver(post_delete, sender=GameData)
